bb9c8d87005522529c21e833ac02118d519bda1f
kkochie <kristinekochie@gmail.com>
2023-04-17 20:16:21 -0400

    Create slider component

--- src/Components/Common/SliderComp.js
+++ src/Components/Common/SliderComp.js
@@ -3,6 +3,7 @@ import Slider from "@mui/material/Slider"
 import { Typography } from "@mui/material"
 import { Stack } from "@mui/system"
 
+// Destructure props
 const SliderComp = ({
   defaultValue,
   min,
@@ -15,9 +16,37 @@ const SliderComp = ({
   steps
 }) => {
   return (
+    // Using MUI Stack component to stack comps vertically.
+    // my is shorthand for marginY [margin-top & margin-bottom].
     <Stack my={1.4}>
-
+      <Stack gap={1}>
+        <Typography variant="subtitle2">{label}</Typography>
+        <Typography variant="h5">
+          {unit} {amount}
+        </Typography>
+      </Stack>
+      {/* Using MUI Slider component to display sliders */}
+      <Slider
+        min={min}
+        max={max}
+        defaultValue={defaultValue}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        onChange={onChange}
+        value={value}
+        marks
+        step={steps}
+      />
+      <Stack direction="row" justifyContent="space-between">
+        <Typography variant="caption" color="text.secondary">
+          {unit} {min}
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          {unit} {max}
+        </Typography>
+      </Stack>
     </Stack>
+
   )
 }
 
