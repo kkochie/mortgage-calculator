29b1f5c6096f22daa6bfc9ad7b5e91133fc1fa0b
kkochie <kristinekochie@gmail.com>
2023-04-17 20:12:59 -0400

    Customize MUI palette

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,14 @@ import { createTheme } from '@mui/material/styles'
 
 export const theme = createTheme({
   palette: {
-    mode: 'dark',
+    primary: {
+      // Teal green
+      main: '#264653',
+    },
+    secondary: {
+      // Orange
+      main: '#e76f51',
+    },
   },
 })
 
