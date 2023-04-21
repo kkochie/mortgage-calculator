914eda1fc6f2545e136ee04c2bc547dc2973cec5
kkochie <kristinekochie@gmail.com>
2023-04-20 22:23:12 -0400

    Set default values for sliders

--- src/App.js
+++ src/App.js
@@ -7,17 +7,26 @@ import SliderSelect from "./Components/SliderSelect"
 import LoanTermSelect from "./Components/LoanTermSelect"
 
 export default function App() {
+  // Set default values for sliders
+  const [ data, setData ] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * .02,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5,
+  })
+
   return (
     <div className="App">
       <Navbar />
       <Container maxWidth="xl" sx={{marginTop:4}}>
         <Grid container spacing={5} alignItems="center">
           <Grid item xs={12} md={6}>
-            {/* <SliderSelect />
-            <LoanTermSelect /> */}
+            <SliderSelect  data={data} setData={setData}/>
+            <LoanTermSelect />
           </Grid>
           <Grid item xs={12} md={6}>
-            {/* <Result /> */}
+            <Result />
           </Grid>
         </Grid>
       </Container>
